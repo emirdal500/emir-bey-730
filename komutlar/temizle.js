9873a9f64b438367f35f354911a18a8c8d24cccd
emirdal500 <45372886+emirdal500@users.noreply.github.com>
2018-11-27 01:40:34 +0300

    Update temizle.js

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -6,7 +6,7 @@ exports.run = function(client, message, args) {
   }
   let mesajsayisi = parseInt(args.join(' '));
   if (mesajsayisi.length < 1) return message.channel.send('Kaç mesaj silmem gerektiğini belirtmedin.')
-  if (mesajsayisi > 100) return message.channel.send('100 adetden fazla mesaj silemem!');
+  if (mesajsayisi > 1000) return message.channel.send('1000 adetden fazla mesaj silemem!');
   message.channel.bulkDelete(mesajsayisi + 1);
   message.channel.send(mesajsayisi +' adet mesaj sildim!')
 };
