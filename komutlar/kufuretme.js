be20760b1553a745fd9529bfbac931bb003a4455
emirdal500 <45372886+emirdal500@users.noreply.github.com>
2018-11-27 15:52:08 +0300

    Delete kufuretme.js

--- komutlar/kufuretme.js
+++
@@ -1,7 +0,0 @@
-client.on("message", message => {
-    const kufur = ["küfür","",""];
-    if (kufur.some(word => message.content.includes(word)) ) {
-        message.reply("Küfür Etme! :rage:")
-        message.delete()
-    }
-});
